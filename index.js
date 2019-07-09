9bb1c4a3c2fa3fa88675db7768c5e83506ed599d
GhostlyDilemma <ghostly.dilemma@gmail.com>
2019-07-09 16:19:53 +0200

    Add Checking for non existant files/folders

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const { Plugin } = require("powercord/entities")
 const { React } = require("powercord/webpack")
-const { sleep } = require('powercord/util')
 const fs = require('fs')
 const path = require('path')
-const { inject, uninject } = require("powercord/injector")
 const Settings = require('./reactcomponents/Settings')
 
 module.exports = class DevInjector extends Plugin {
-  constructor () {
+  constructor() {
     super()
-      // Declare class wide variable to reuse
-      this.styleManager = undefined
-      this.idCounter = 0
-      this.idStorage = []
-      this.loadedFiles = []
+    // Declare class wide variable to reuse
+    this.idCounter = 0
+    this.idStorage = []
+    this.loadedFiles = []
   }
   async startPlugin() {
-    this.styleManager = powercord.pluginManager.get('pc-styleManager')
-
-    while (!this.styleManager.ready) {
-      await sleep(1)
-    }
     // Load Internal Styling
-    this.styleManager.loadCSS(`Customa-Injector-Styles`, path.join(__dirname, 'style.css'))
+    this.loadCSS(`Customa-Injector-Styles`, path.join(__dirname, 'style.css'))
     // Register Settings Menu
     this.registerSettings(
       'devInjector',
       'Customa Dev Injector',
       (props) => React.createElement(Settings, { ...props, saveHandler: this.handleSave.bind(this) })
     )
-
-    // Wait until the StyleManager is Ready
 
 
     // Load Files and Folders with Default Parameters
@@ -63,7 +53,7 @@ module.exports = class DevInjector extends Plugin {
     // When nothing is set in settings this field can be undefined, to not break the loop below, it will be set to an empty array
     if (!filenames) {
       // End the function as we haven't received anything of use
-      return void 0;
+      return void 0
     } else {
       // To be able to use .includes later in the code, all exceptions will be formed according to the filename
       exceptions = exceptions.map((exception) => {
@@ -74,7 +64,7 @@ module.exports = class DevInjector extends Plugin {
     // Loop through all the files
     filenames.filter(e => e.value).forEach(element => {
       // Gather Value from field
-      let filename = element.value;
+      let filename = element.value
 
       // Is "folder" not empty or does it already end with a \
       if (folder && (!folder.endsWith('\\') || !folder.endsWith('/'))) {
@@ -87,33 +77,37 @@ module.exports = class DevInjector extends Plugin {
 
       // If File is exceptions ignore it
       if (exceptions.includes(cleanFilename)) {
-        this.log("Excluded File: " + filename)
+        this.info(`Excluded File: ${filename}`)
       } else if (this.loadedFiles.includes(cleanFilename)) {
-        this.log("Duplicate File: " + filename)
+        this.warn(`Duplicate File: ${filename}`)
       } else {
         // Check if "File" is really a file
-        if (fs.lstatSync(filename).isFile()) {
-          // Check if the file is a css file
-          if (filename.endsWith('css')) {
-            // Create an ID (using the counter) and the filename (without filetype)
+        try {
+          if (fs.lstatSync(filename).isFile()) {
+            // Check if the file is a css file
+            if (filename.endsWith('css')) {
+              // Create an ID (using the counter) and the filename (without filetype)
 
-            filename = filename.replace(/\\/g, '/')
+              filename = filename.replace(/\\/g, '/')
 
-            let id = `${this.idCounter}-${filename.split("/").slice(-1)[0].split('.')[0]}`
-            this.idCounter++
+              let id = `${this.idCounter}-${filename.split("/").slice(-1)[0].split('.')[0]}`
+              this.idCounter++
 
-            // Push the new ID to the storage
-            this.idStorage.push(id)
+              // Push the new ID to the storage
+              this.idStorage.push(id)
 
-            // Filename added to loaded files for internal checking
-            this.loadedFiles.push(filename)
+              // Filename added to loaded files for internal checking
+              this.loadedFiles.push(filename)
 
-            // Load File
-            this.styleManager.loadCSS(`Customa-Injector-File-${id}`, cleanFilename)
+              // Load File
+              this.loadCSS(`Customa-Injector-File-${id}`, cleanFilename)
+            }
+          } else {
+            // In case nested loading is needed, the current "file" will be sent to the folder loading again
+            this.loadFolder([{ key: 0, value: filename }])
           }
-        } else {
-          // In case nested loading is needed, the current "file" will be sent to the folder loading again
-          this.loadFolder([{ key: 0, value: filename }])
+        } catch (e) {
+          this.error(`File not found: ${filename}`)
         }
       }
     })
@@ -123,7 +117,7 @@ module.exports = class DevInjector extends Plugin {
     // When nothing is set in settings this field can be undefined, to not break the loop below, it will be set to an empty array
     if (!foldernames) {
       // End function because we are not actually relieving anything of meaning
-      return void 0;
+      return void 0
     } else {
       // To be able to use .includes later in the code, all exceptions will be formed according to the filename
       exceptions = exceptions.map((exception) => {
@@ -138,23 +132,27 @@ module.exports = class DevInjector extends Plugin {
 
       // Ignore the folder when it is in the exceptions
       if (!exceptions.includes(foldername)) {
-        // Check whether the "folder", is actually a folder
-        if (!fs.lstatSync(foldername).isFile()) {
+        try {
+          // Check whether the "folder", is actually a folder
+          if (!fs.lstatSync(foldername).isFile()) {
 
-          // Read all Files into loadFiles when it is a folder, use default exceptions but give current folder as parameter so file will be found
-          let fileNames = fs.readdirSync(foldername);
+            // Read all Files into loadFiles when it is a folder, use default exceptions but give current folder as parameter so file will be found
+            let fileNames = fs.readdirSync(foldername)
 
-          // Prepare Array for call of loadFile
-          let files = fileNames.map((file) => {
-            return { key: 0, value: file }
-          })
-          this.loadFiles(files, undefined, foldername)
-        } else {
-          // Send one element array to file Loading when "folder" is file
-          this.loadFiles({ key: 0, value: foldername })
+            // Prepare Array for call of loadFile
+            let files = fileNames.map((file) => {
+              return { key: 0, value: file }
+            })
+            this.loadFiles(files, undefined, foldername)
+          } else {
+            // Send one element array to file Loading when "folder" is file
+            this.loadFiles({ key: 0, value: foldername })
+          }
+        } catch (e) {
+          this.error(`Folder not found: ${foldername}`)
         }
       } else {
-        this.log("Excluded Folder: " + foldername)
+        this.info(`Excluded Folder: ${foldername}`)
       }
     })
   }
